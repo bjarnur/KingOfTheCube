d78564c34d4735c003ccc8e9d3ac92b14b172a10
NoNiMad <nonimad@free.fr>
2018-09-11 16:41:11 +0200

    Working image recognition and Cube popping on top
    The image used is the cover on my Ready Player One book, we should work on a temporary better marker quickly

---
+++ Assets/Scripts/CubeVisualizer.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using GoogleARCore;
+using GoogleARCoreInternal;
+using UnityEngine;
+
+public class CubeVisualizer : MonoBehaviour
+{
+    public AugmentedImage Image;
+    public GameObject Cube;
+    
+    public void Update()
+    {
+        if (Image == null || Image.TrackingState != TrackingState.Tracking)
+        {
+            Cube.SetActive(false);
+            return;
+        }
+
+        float halfWidth = Image.ExtentX / 2;
+        float halfHeight = Image.ExtentZ / 2;
+        Cube.transform.localPosition = Vector3.zero;
+        //Cube.transform.localPosition = (halfWidth * Vector3.left) + (halfHeight * Vector3.back);
+        /*
+        FrameLowerRight.transform.localPosition = (halfWidth * Vector3.right) + (halfHeight * Vector3.back);
+        FrameUpperLeft.transform.localPosition = (halfWidth * Vector3.left) + (halfHeight * Vector3.forward);
+        FrameUpperRight.transform.localPosition = (halfWidth * Vector3.right) + (halfHeight * Vector3.forward);
+        */
+
+        Cube.SetActive(true);
+        /*
+        FrameLowerRight.SetActive(true);
+        FrameUpperLeft.SetActive(true);
+        FrameUpperRight.SetActive(true);
+        */
+    }
+}
---
+++ Assets/Scripts/Visualizer.cs
@@ -0,0 +1,69 @@
+﻿//-----------------------------------------------------------------------
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Visualizer : MonoBehaviour
+{
+    public CubeVisualizer CubeVisualizerPrefab;
+
+    public GameObject TextUI;
+
+    private Dictionary<int, CubeVisualizer> m_Visualizers
+        = new Dictionary<int, CubeVisualizer>();
+
+    private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+
+    public void Update()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Check that motion tracking is tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        // Get updated augmented images for this frame.
+        Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
+
+        // Create visualizers and anchors for updated augmented images that are tracking and do not previously
+        // have a visualizer. Remove visualizers for stopped images.
+        foreach (var image in m_TempAugmentedImages)
+        {
+            CubeVisualizer visualizer = null;
+            m_Visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+            if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+            {
+                // Create an anchor to ensure that ARCore keeps tracking this augmented image.
+                Anchor anchor = image.CreateAnchor(image.CenterPose);
+                visualizer = (CubeVisualizer)Instantiate(CubeVisualizerPrefab, anchor.transform);
+                visualizer.Image = image;
+                m_Visualizers.Add(image.DatabaseIndex, visualizer);
+            }
+            else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+            {
+                m_Visualizers.Remove(image.DatabaseIndex);
+                GameObject.Destroy(visualizer.gameObject);
+            }
+        }
+
+        // Show the fit-to-scan overlay if there are no images that are Tracking.
+        foreach (var visualizer in m_Visualizers.Values)
+        {
+            if (visualizer.Image.TrackingState == TrackingState.Tracking)
+            {
+                TextUI.SetActive(false);
+                return;
+            }
+        }
+
+        TextUI.SetActive(true);
+    }
+}
