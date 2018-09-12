974630b3b8a23d5e9281b27268fa05ae497c9a3e
Bjarni <bjarniragnarg@gmail.com>
2018-09-12 17:23:56 +0200

    Improved stability in movement and code refactor

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,8 +7,16 @@ public class CharacterController : MonoBehaviour {
     Animator animator;
     Rigidbody rb;
 
+    int side = 0;
+
     float speed = 2f;
     float angle = 0;
+
+    //TODO: Set these values appropriately with respect to level dimensions
+    const float xBoundsMin = -4.5f;
+    const float xBoundsMax = 4.5f;
+    const float zBoundsMin = -4.5f;
+    const float zBoundsMax = 4.5f;
 
     bool climbing = false;
     bool goingRight = false;
@@ -17,17 +25,132 @@ public class CharacterController : MonoBehaviour {
     void Start () {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-	}
-	
+	}	
 	
 	void Update () {
 
+        //Ensure we only travel in the appropriate dimensions
+        ensureConsistentMovement();
+
+        //Trigger animations based on user input
+        triggerAnimations();
+
+        //Update character position and rotation
+        updateCharacterPosition();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            Debug.Log("Ladder enter");
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            rb.useGravity = false;
+            climbing = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            Debug.Log("Ladder exit");
+            rb.useGravity = true;
+            climbing = false;
+        }
+    }
+
+
+    /*********************************\
+        Helper and utility functions
+    \*********************************/ 
+
+    void ensureConsistentMovement()
+    {
+        switch (side)
+        {
+            case 0:
+                transform.position = new Vector3(transform.position.x, transform.position.y, 4.5f);
+                break;
+            case 1:
+                transform.position = new Vector3(4.5f, transform.position.y, transform.position.z);
+                break;
+            case 2:
+                transform.position = new Vector3(transform.position.x, transform.position.y, -4.5f);
+                break;
+            case 3:
+                transform.position = new Vector3(-4.5f, transform.position.y, transform.position.z);
+                break;
+        }
+    }
+
+    void updateCharacterPosition()
+    {
+        //Update character position, and rotation around the cube while left arrow button is held down
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > xBoundsMax && equals(transform.position.z, zBoundsMax, 0.001f))
+            {
+                crossAngle(xBoundsMax, zBoundsMax, true);
+            }
+            if (equals(transform.position.x, xBoundsMax, 0.001f) && transform.position.z < zBoundsMin)
+            {
+                crossAngle(xBoundsMax, zBoundsMin, true);
+            }
+            if (equals(transform.position.z, zBoundsMin, 0.001f) && transform.position.x < xBoundsMin)
+            {
+                crossAngle(xBoundsMin, zBoundsMin, true);
+            }
+            if (equals(transform.position.x, xBoundsMin, 0.001f) && transform.position.z > zBoundsMax)
+            {
+                crossAngle(zBoundsMin, zBoundsMax, true);
+            }
+
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+
+        //Update character position, and rotation around the cube while right arrow button is held down
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.position.x < xBoundsMin && equals(transform.position.z, zBoundsMax, 0.001f))
+            {
+                crossAngle(xBoundsMin, zBoundsMax, false);
+            }
+            if (equals(transform.position.x, xBoundsMin, 0.001f) && transform.position.z < zBoundsMin)
+            {
+                crossAngle(xBoundsMin, zBoundsMin, false);
+            }
+            if (equals(transform.position.z, zBoundsMin, 0.001f) && transform.position.x > xBoundsMax)
+            {
+                crossAngle(xBoundsMax, zBoundsMin, false);
+            }
+            if (equals(transform.position.x, xBoundsMax, 0.001f) && transform.position.z > zBoundsMax)
+            {
+                crossAngle(xBoundsMax, zBoundsMax, false);
+            }
+
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) && climbing)
+        {
+            transform.position += transform.up * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) && climbing)
+        {
+            transform.position -= transform.up * Time.deltaTime * speed;
+        }
+    }
+
+    void triggerAnimations()
+    {
         //Update animations and rotation the instance a button is pressed down
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             animator.SetBool("Run", true);
             animator.SetBool("Stop", false);
-            if(Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 goingRight = true;
                 angle -= 90;
@@ -56,9 +179,9 @@ public class CharacterController : MonoBehaviour {
         {
             //if (climbing)
             //{
-                transform.localEulerAngles = new Vector3(0f, 180, 0f);
-                animator.SetBool("Climb", false);
-                animator.SetBool("StopClimbing", true);
+            transform.localEulerAngles = new Vector3(0f, 180, 0f);
+            animator.SetBool("Climb", false);
+            animator.SetBool("StopClimbing", true);
             //}
         }
 
@@ -67,7 +190,7 @@ public class CharacterController : MonoBehaviour {
         {
             animator.SetBool("Run", false);
             animator.SetBool("Stop", true);
-            if(goingRight)
+            if (goingRight)
             {
                 goingRight = false;
                 angle += 90;
@@ -79,111 +202,22 @@ public class CharacterController : MonoBehaviour {
                 transform.localEulerAngles = new Vector3(0f, angle, 0f);
             }
         }
-       
-        //Update character position, and rotation around the cube while left arrow button is held down
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (transform.position.x > 4.5f && equals(transform.position.z, 4.5f, 0.001f))
-            {
-                angle += 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);
-                transform.position = new Vector3(4.5f, 1f, 4.5f);
-            }
-            if (equals(transform.position.x, 4.5f, 0.001f) && transform.position.z < -4.5f)
-            {
-                transform.localEulerAngles = new Vector3(0f, 0, 0f);
-                transform.position = new Vector3(4.5f, 1f, -4.5f);
-                angle += 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);                
-            }
-            if (equals(transform.position.z, -4.5f, 0.001f) && transform.position.x < -4.5f)
-            {                
-                transform.localEulerAngles = new Vector3(0f, 0, 0f);
-                transform.position = new Vector3(-4.5f, 1f, -4.5f);
-                angle += 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);                
-            }
-            if (equals(transform.position.x, -4.5f, 0.001f) && transform.position.z > 4.5f)
-            {
-                transform.localEulerAngles = new Vector3(0f, 0, 0f);
-                transform.position = new Vector3(-4.5f, 1f, 4.5f);
-                angle += 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);                
-            }
-
-            transform.position += transform.forward * Time.deltaTime * speed;
-        }
-
-        //Update character position, and rotation around the cube while right arrow button is held down
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if (transform.position.x < -4.5f && equals(transform.position.z, 4.5f, 0.001f))
-            {
-                transform.localEulerAngles = new Vector3(0f, 0, 0f);
-                transform.position = new Vector3(-4.5f, 1f, 4.5f);
-                angle -= 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);
-            }
-            if (equals(transform.position.x, -4.5f, 0.001f) && transform.position.z < -4.5f)
-            {
-                transform.localEulerAngles = new Vector3(0f, 0, 0f);
-                transform.position = new Vector3(-4.5f, 1f, -4.5f);
-                angle -= 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);                
-            }            
-            if (equals(transform.position.z, -4.5f, 0.001f) && transform.position.x > 4.5f)
-            {
-                transform.localEulerAngles = new Vector3(0f, 0, 0f);
-                transform.position = new Vector3(4.5f, 1f, -4.5f);
-                angle -= 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);
-            }
-            if (equals(transform.position.x, 4.5f, 0.001f) && transform.position.z > 4.5f)
-            {
-                transform.localEulerAngles = new Vector3(0f, 0, 0f);
-                transform.position = new Vector3(4.5f, 1f, 4.5f);
-                angle -= 90;
-                transform.localEulerAngles = new Vector3(0f, angle, 0f);
-            }
-
-            transform.position += transform.forward * Time.deltaTime * speed;
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow) && climbing)
-        {
-            transform.position += transform.up * Time.deltaTime * speed;
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow) && climbing)
-        {
-            transform.position -= transform.up * Time.deltaTime * speed;
-        }
     }
+    
 
-
-    void OnTriggerEnter(Collider other)
+    private void crossAngle(float xPos, float zPos, bool fromRight)
     {
-        if (other.gameObject.CompareTag("Ladder"))
-        {
-            Debug.Log("Ladder enter");
-            rb.useGravity = false;
-            climbing = true;
+        if(fromRight) {
+            side = (side + 1) % 4;
+            angle += 90;            
         }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Ladder"))
-        {
-            Debug.Log("Ladder exit");
-            rb.useGravity = true;
-            climbing = false;
+        else {
+            side = (side - 1) % 4;
+            angle -= 90;
         }
+        transform.localEulerAngles = new Vector3(0f, angle, 0f);
+        transform.position = new Vector3(xPos, transform.position.y, zPos);        
     }
-
-    /*********************************\
-        Helper and utility functions
-    \*********************************/ 
 
     private bool equals(float a, float b, float err)
     {
