44e9444433cfcb0ebe1799c6ee79bf17d7796f0e
NoNiMad <nonimad@free.fr>
2018-09-13 16:24:10 +0200

    New ARController with better logic and cleaner code

---
+++ Assets/Scripts/ARController.cs
@@ -0,0 +1,74 @@
+﻿//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARController : MonoBehaviour
+{
+    // 20 centimeters wide
+    public static float RealCubeSize = 0.2f;
+
+    public Transform WorldContainer;
+    public BoxCollider VirtualCubeCollider;
+    public GameObject UIScanning;
+
+    private List<AugmentedImage> m_AugmentedImages = new List<AugmentedImage>();
+    private AugmentedImage KOTCImage = null;
+    private Anchor KOTCAnchor = null;
+
+    public void Start()
+    {
+        Vector3 colliderSize = VirtualCubeCollider.size;
+        Vector3 scale = new Vector3(RealCubeSize / colliderSize.x, RealCubeSize / colliderSize.y, RealCubeSize / colliderSize.z) * 2f;
+        WorldContainer.localScale = scale;
+    }
+
+    public void Update()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Check that motion tracking is tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        // Get updated augmented images for this frame.
+        Session.GetTrackables<AugmentedImage>(m_AugmentedImages, TrackableQueryFilter.Updated);
+
+        foreach (var image in m_AugmentedImages)
+        {
+            // The KOTC Marker
+            if(image.DatabaseIndex == 0)
+            {
+                if (image.TrackingState == TrackingState.Tracking && KOTCImage == null)
+                {
+                    KOTCImage = image;
+                    KOTCAnchor = image.CreateAnchor(image.CenterPose);
+
+                    WorldContainer.parent = KOTCAnchor.transform;
+                    WorldContainer.localPosition = Vector3.zero;
+                    WorldContainer.localRotation = Quaternion.identity;
+                    WorldContainer.gameObject.SetActive(true);
+                } else if (image.TrackingState == TrackingState.Stopped)
+                {
+                    WorldContainer.parent = null;
+                    WorldContainer.gameObject.SetActive(false);
+
+                    KOTCImage = null;
+                    GameObject.Destroy(KOTCAnchor);
+                    KOTCAnchor = null;
+                }
+            }
+        }
+
+        UIScanning.SetActive(KOTCImage == null);
+    }
+}
---
+++ Assets/Scripts/MobileController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileController : MonoBehaviour {
+
+    public Transform VirtualCube;
+    public CharacterController Character;
+	
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("#########\nTOUCHED\n#########");
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            //Character.GetComponent<Rigidbody>().MovePosition(VirtualCube.position + Vector3.up);
+        }
+    }
+}
