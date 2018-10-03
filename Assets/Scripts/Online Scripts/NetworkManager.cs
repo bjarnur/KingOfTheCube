649dbb4cbfec1afc7ce6d1f197826084a067cccc
Bjarni <bjarniragnarg@gmail.com>
2018-10-03 16:04:55 +0200

    First go at multiplayer (not in AR so far)

---
+++ Assets/Scripts/Online Scripts/NetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    private int playerNumber = 1;
+    const string VERSION = "0.0.1";
+    public string roomName = "myRoom";
+    public GameObject player;
+    public GameObject garden;
+    public Transform spawn;
+    //public string playerPrefabName = "character";
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("STARTING UP");
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+        PhotonNetwork.autoJoinLobby = true;
+	}
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("JOINED LOBBY");
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 5 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+	
+	void OnJoinedRoom()
+    {
+        Debug.Log("JOINING ROOM");
+
+        //Debug.Log("player count " + PhotonNetwork.playerList.Length);
+        //Transform loc = garden.transform.GetChild(PhotonNetwork.playerList.Length);
+        PhotonNetwork.Instantiate("Player", spawn.position, Quaternion.identity, 0);        
+    }
+}
---
+++ Assets/Scripts/Online Scripts/NetworkPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(photonView.isMine)
+        {
+            GetComponent<PlayerController_AssemCube>().enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
