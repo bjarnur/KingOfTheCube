4cea894c83d0f024801d879f52c3a41a520f8a8f
pooryag <poorya@gmail.com>
2018-10-09 15:52:39 +0200

    added a prefab platform that shatters

---
+++ Assets/Scripts/ShatterOnImpact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterOnImpact : MonoBehaviour {
+
+    public GameObject shattered;
+
+    public float timer = 3f;
+
+
+    private void OnMouseDown( )
+    {
+
+        GameObject.Instantiate(shattered, transform.position, transform.rotation);
+
+        Destroy(this.gameObject);
+    }
+}
